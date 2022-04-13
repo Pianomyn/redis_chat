be0099b49465372a32c72ca2d06f0bcca188b24c
Andi <anditan2014@gmail.com>
2022-04-14 09:06:33 +1000

    asdf

--- publisher.py
+++ publisher.py
@@ -3,5 +3,13 @@ import redis
 r = redis.Redis()
 
 while True:
-    val = input()
-    r.publish("chat", val)
+    username = input("Please enter your username: ")
+    if not r.sismember("usernames", username):
+        r.sadd("usernames", username)
+        break
+    else:
+        print("This username is already taken!")
+
+while True:
+    val = input("Please enter your message: ")
+    r.publish("chat", "{}: {}".format(username, val))
